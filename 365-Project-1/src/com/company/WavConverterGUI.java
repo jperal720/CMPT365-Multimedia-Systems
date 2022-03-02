89a3902663e3e0bf6ac1fca2c16714a12090842c
Jonathan <jperalgo@sfu.ca>
2022-03-02 10:38:14 -0800

    Added Project-1

---
+++ 365-Project-1/src/com/company/WavConverterGUI.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+public class WavConverterGUI extends JFrame{
+    private JPanel mainPanel;
+
+    public WavConverterGUI(String appName){
+        super(appName);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
