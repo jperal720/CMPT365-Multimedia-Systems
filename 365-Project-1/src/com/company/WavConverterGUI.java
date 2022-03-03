5cd27c7d510a48fd9dfdf3b8ddbb64c7fa243d34
Jonathan <jperalgo@sfu.ca>
2022-03-03 10:25:55 -0800

    Finished open file feature

--- 365-Project-1/src/com/company/WavConverterGUI.java
+++ 365-Project-1/src/com/company/WavConverterGUI.java
@@ -1,14 +1,58 @@
 package com.company;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.*;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 public class WavConverterGUI extends JFrame{
     private JPanel mainPanel;
+    private JButton selectButton;
+    private JFileChooser openFileChooser;
+    private AudioInputStream wavFile;
+
 
     public WavConverterGUI(String appName){
         super(appName);
 
+        openFileChooser = new JFileChooser();
+        openFileChooser.setCurrentDirectory(new File("/Users/jonathanperalgort/Documents"));
+
+        this.setPreferredSize(new Dimension(600, 400));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+
+
+        selectButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int returnValue = openFileChooser.showOpenDialog(mainPanel);
+
+                if (returnValue == openFileChooser.APPROVE_OPTION){
+                    try{
+                        wavFile = AudioSystem.getAudioInputStream(openFileChooser.getSelectedFile());
+                        Clip clip = AudioSystem.getClip();
+                        clip.open(wavFile);
+                        clip.start();
+                    }catch(Exception ioe){
+                        System.out.println("Error: could not load file");
+                    }
+                }
+            }
+        });
     }
 
+    public static void main(String[] args) {
+        JFrame frame = new WavConverterGUI("WAV Plot");
+        frame.setVisible(true);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
