3ae4364580f1af343ed831db1e95d287644df1f4
Jonathan <jperalgo@sfu.ca>
2022-04-11 18:29:15 -0700

    Finished Programming assignment 3

--- prog_assignment3/src/main.java
+++ prog_assignment3/src/main.java
@@ -15,30 +15,72 @@ public class main {
         String st;
 
         while((st = br.readLine()) != null){
-            System.out.println(st);
+            String[] strArr = st.split(" ");    //Adding the space-delimited values into the LinkedList<Float>
+            for(String a : strArr)
+                input.addLast(Float.valueOf(a).floatValue());
         }
 
+        /**
+         * Displaying Input Values
+         */
+        System.out.println("Input values:");
+        LinkedList<LinkedList<Float>> matrix = convertInputToList(input, N);
+        displayMatrix(matrix, N);
 
+        /**
+         * Creating transformed and transposed matrices
+         */
+        LinkedList<LinkedList<Float>> transformedMatrix = transform(N); //Transform Matrix
+        LinkedList<LinkedList<Float>> transposedMatrix = transpose(transformedMatrix, N);  //Transposing Matrix
 
+        /**
+         * Row Transform first!
+         */
+        LinkedList<LinkedList<Float>> finalMatrix = matrixMult(matrix, transposedMatrix, N);
+        finalMatrix = matrixMult(transformedMatrix, finalMatrix, N);
+        System.out.println("Row Transform First:");
 
-        //Test Case for transform functions
-//        LinkedList<LinkedList<Float>> arr = transform(N);
-//        for(LinkedList<Float> arr1 : arr)
-//            for(Float fl : arr1)
-//                System.out.println(fl);
+        /**
+         * Displaying finalMatrix
+         */
+        displayMatrix(finalMatrix, N);
+
+        /**
+         * Column Transform first!
+         */
+        finalMatrix = matrixMult(transformedMatrix, matrix, N);
+        finalMatrix = matrixMult(transposedMatrix, finalMatrix, N);
+        System.out.println("Column Transform First: ");
+
+        /**
+         * Displaying finalMatrix
+         */
+
+        displayMatrix(finalMatrix, N);
+
+    }
+
+    public static void displayMatrix(LinkedList<LinkedList<Float>> matrix, int N){
+        for(int i = 0; i < matrix.size(); i++){
+            for(int x = 0; x < matrix.size(); x++) {
+                System.out.print(Math.round((matrix.get(i)).get(x)) + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     public static LinkedList<LinkedList<Float>> matrixMult(LinkedList<LinkedList<Float>> a, LinkedList<LinkedList<Float>> b, int N) {
         LinkedList<LinkedList<Float>> output = new LinkedList<LinkedList<Float>>();
 
         for (int i = 0; i < N; i++) {
-            output.push(new LinkedList<Float>());
+            output.addLast(new LinkedList<Float>());
             for (int x = 0; x < N; x++) {
                 float total = 0;
                 for (int j = 0; j < N; j++)
                     total+= (float)a.get(i).get(j) * (float)b.get(j).get(x);
                 if(total > -0.5 && total < 0) total *= -1;
-                output.get(i).push(total);
+                output.get(i).addLast(total);
             }
         }
 
@@ -49,10 +91,9 @@ public class main {
         LinkedList<LinkedList<Float>> output = new LinkedList<LinkedList<Float>>();
 
         for(int i = 0; i < N; i++){
-            output.push(new LinkedList<Float>());
+            output.addLast(new LinkedList<Float>());
             for (int x = 0; x < N; x++) {
-                int indx = N * i + x;
-                output.get(i).push(input.get(indx));
+                output.get(i).addLast(input.get(x * N + i));
             }
         }
 
@@ -63,10 +104,10 @@ public class main {
         LinkedList<LinkedList<Float>> transposeInput = new LinkedList<LinkedList<Float>>();
 
         for(int i = 0; i < N; i++){
-            transposeInput.push(new LinkedList<Float>());
+            transposeInput.addLast(new LinkedList<Float>());
 
             for (int x = 0; x < N; x++) {
-                transposeInput.get(i).push((transformedList.get(x)).get(i));    //Pusing [x][i] unto transposeInput List
+                transposeInput.get(i).addLast((transformedList.get(x)).get(i));    //Adding [x][i] unto transposeInput List
             }
 
         }
@@ -80,13 +121,13 @@ public class main {
 
         for(int i = 0; i < N; i++){
             float a = i == 0 ? (float) (1 / Math.sqrt(N)) : (float) (2 / Math.sqrt(N));
-            transformInput.push(new LinkedList<Float>());
+            transformInput.addLast(new LinkedList<Float>());
 
             for(int x = 0; x < N; x++){
                 float calcValue = (float)(x*2 + 1); calcValue = (float) (Math.PI * i * calcValue);
                 calcValue = (float)(calcValue/(2*N)); calcValue = (float) Math.cos(calcValue);
                 calcValue = (float) (x * calcValue);
-                transformInput.get(i).push(calcValue);
+                transformInput.get(i).addLast(calcValue);
             }
         }
 
